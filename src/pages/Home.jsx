4f443bc5b0ff4ef58fcb99e50b4511d28bd0a486
sabina701 <shresthasabeena9@gmail.com>
2026-08-18 17:04:15 +0545

    18th August

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Header from "../components/header/Header";
-import Footer from "../components/Footer/footer";
+
 import Carousel from "../components/Home/Carousel";
 import FlashSales from "../components/flashsales/FlashSales";
 const Home = () => {
   return (
     <>
-  
       <Carousel />
 
       <FlashSales />
-  
     </>
   );
 };
